133306286e9f459f71cf3f012ef677b19c498bf9
Luca Gorayeb <lucagorayeb@gmail.com>
2025-10-20 13:16:16 -0400

    updating

--- clinica/urls.py
+++ clinica/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='clinica-about'),
     path('professional/', views.professional , name='clinica-professional'),
     path('desease/', views.desease, name='clinica-desease'),
+    path('desease/cirurgia-traumatologia', views.desease, name='clinica-desease-cirurgia-traumatologia'),
 ]
