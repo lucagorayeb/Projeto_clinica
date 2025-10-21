0fd4e993fe6e133e1fc35a4542f4d01cc68ef580
Luca Gorayeb <lucagorayeb@gmail.com>
2025-10-21 12:55:38 -0400

    updating de public web page

--- clinica/views.py
+++ clinica/views.py
@@ -11,3 +11,14 @@ def desease(request):
 
 def about(request):
     return render(request, 'clinica/public/about.html')
+
+def desease_cirurgia_traumatologia(request):
+    return render(request, 'clinica/public/desease/cirugia_traumatologia.html')
+
+def desease_periconarite(request):
+    return render(request, 'clinica/public/desease/periconarite.html')
+
+def desease_dentes_inclusos(request):
+    return render(request, 'clinica/public/desease/dentes_inclusos.html')
+
+
