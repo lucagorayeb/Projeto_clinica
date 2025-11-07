5269dec589f88dd0b79205c9cf976e473babafbe
Luca Gorayeb <lucagorayeb@gmail.com>
2025-11-07 00:00:48 -0400

    updating

--- projeto_clinica/settings.py
+++ projeto_clinica/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'projeto_clinica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'teste_clinica',
     }
 }
 
