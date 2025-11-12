8db41b5ae526052ae8de989aec900502fced5799
Luca Siqueira Assis Gorayeb de Mello <lucagorayeb@gmail.com>
2025-11-12 08:11:17 -0400

    update

--- clinica/models.py
+++ clinica/models.py
@@ -1,13 +1,40 @@
 from django.db import models
 
+class SexoPaciente:
+    def  escolhe_sexo():
+        paciente_sexo = {
+            "M": "Masculino",
+            "F": 'Feminino',
+            'O': 'Outro',
+        }
+
+        return paciente_sexo
+
 class Paciente(models.Model):
-    nome = models.CharField(primary_key=True, max_length=100, default='')
-    cpf = models.CharField(max_length=14, default='')
-    rg = models.CharField(max_length=14, default='')
-    data_nascimento = models.DateField()
+    id_paciente = models.AutoField(primary_key=True)
+    nome_paciente = models.CharField(max_length=100, default='')
+    cpf_paciente = models.CharField(max_length=14, default='')
+    rg_paciente = models.CharField(max_length=14, default='')
+    data_nascimento_paciente = models.DateField()
+    sexo_paciente = models.CharField(max_length=1, choices=SexoPaciente.escolhe_sexo())
+    numero_paciente = models.CharField(max_length=16)
 
     def __str__(self):
         return f'''nome: {self.nome}'''
+
+class Endereco(models.Model):
+    id_endereco = models.AutoField(primary_key=True)
+    logradouro_endereco = models.CharField(max_length=100, null=False)
+    numero_endereco = models.IntegerField(null=False)
+    bairro_endereco = models.CharField(max_length=100, null=False)
+    cep_endereco = models.CharField(max_length=9, null=False)
+    cidade_endereco = models.CharField(max_length=100, null=False)
+    estado_endereco = models.CharField(max_length=2, null=False)
+    fk_id_paciente = models.ForeignKey(Paciente, on_delete= models.CASCADE)
+
+    def __str__(self):
+        return f'''CEP: {self.cep_endereco}'''
+
 
 
     
