6e64959b2fc2b81639d935902eca3f49630eda82
Luca Gorayeb <lucagorayeb@gmail.com>
2025-11-18 22:05:24 -0400

    updating

--- clinica/admin.py
+++ clinica/admin.py
@@ -1,10 +1,43 @@
 from django.contrib import admin
-from .models import Paciente, Consulta, Agendamento
-from .form import AgendamentoForm
-
-admin.site.register(Consulta)
-admin.site.register(Paciente)
-admin.site.register(Agendamento)
+from .models import Paciente, Agendamento, Consulta
 
 
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = [ 'nome', 'CPF']
 
+    fieldsets = (
+        ('Dados pessoais', {
+            'fields': ('nome', 'CPF', 'RG', 'Data_Nascimento', 'sexo', 'contato')
+        }),
+        ('Endereço', {
+            'fields': ('logradouro', 'lote', 'bairro', 'CEP', 'cidade', 'estado')
+        }),
+    )
+
+class AgendamentoAdmin(admin.ModelAdmin):
+    list_display = ['paciente_agendamento', 'Data_do_Agendamento', 'Data_da_Consulta']
+
+    fieldsets = (
+        ('Detalhes do Agendamento', {
+            'fields': ('Data_do_Agendamento', 'Tipo_da_Consulta', 'Qual_Plano', 'Carteira_do_Plano',
+                        'Data_da_Consulta', 'Valor_da_Consulta', 'Emitiu_Nota_Fiscal', 'Queixa_do_Paciente',
+                        'paciente_agendamento')
+        }),
+    )
+
+class ConsultaAdmin(admin.ModelAdmin):
+    list_display = ['Consulta_Realizada', 'paciente_consulta']
+
+    fieldsets = (
+        ('Detalhes da Consulta', {
+            'fields': ('Consulta_Realizada', 'Paciente_Trouxe_Exames', 'Exames_Trazidos',
+                        'Laudos_dos_Exames_Trazidos', 'Achados_do_Primeiro_Check_UP', 'Conduta',
+                        'Encaminhado_para_Outro_Profissional', 'Profissional_Encaminhado',
+                        'Estimativa_da_Diagnose', 'paciente_consulta')
+        }),
+    )
+
+
+admin.site.register(Paciente, PacienteAdmin)
+admin.site.register(Agendamento, AgendamentoAdmin)
+admin.site.register(Consulta, ConsultaAdmin)
--- clinica/models.py
+++ clinica/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 from multiselectfield import MultiSelectField
 from .enums import SexoPaciente, SimOuNao, ParticularOuConvenio, PlanosDeSaude, ConsultaRealizada, Exames, ProfissionaisParaEncaminhar
+
+class Paciente(models.Model):
+
+    id_paciente = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100, default='')
+    CPF = models.CharField(max_length=14, default='')
+    RG = models.CharField(max_length=14, default='')
+    Data_Nascimento = models.DateField()
+    sexo = models.CharField(max_length=1, choices=SexoPaciente, default=SexoPaciente.outro)
+    contato = models.CharField(max_length=16)
+    logradouro = models.CharField(max_length=100, null=False)
+    lote = models.IntegerField(null=False)
+    bairro = models.CharField(max_length=100, null=False)
+    CEP = models.CharField(max_length=9, null=False)
+    cidade = models.CharField(max_length=100, null=False)
+    estado = models.CharField(max_length=2, null=False)
+
+    def __str__(self):
+        return f'{self.nome}'
     
 class Consulta(models.Model):
     id_consulta = models.AutoField(primary_key=True)
@@ -13,39 +32,28 @@ class Consulta(models.Model):
     Encaminhado_para_Outro_Profissional = models.CharField(max_length=5, choices=SimOuNao)
     Profissional_Encaminhado = models.CharField(max_length=70, choices=ProfissionaisParaEncaminhar)
     Estimativa_da_Diagnose = models.CharField(max_length=2000)
+    paciente_consulta = models.ForeignKey(Paciente, on_delete=models.CASCADE, null=True, blank=True)
+    def __str__(self):
+        return f'{self.paciente_consulta}'
 
 
     
 class Agendamento(models.Model):
     id_agendamento = models.AutoField(primary_key=True)
-    Data_da_Consulta = models.DateField()
+    Data_do_Agendamento = models.DateField()
     Tipo_da_Consulta = models.CharField(max_length=15, choices=ParticularOuConvenio)
     Qual_Plano = models.CharField(max_length=15, choices=PlanosDeSaude, null=True)
     Carteira_do_Plano = models.CharField(max_length=15, null=True)
+    Data_da_Consulta = models.DateField()
     Valor_da_Consulta = models.DecimalField(decimal_places=2, max_digits=6)
     Emitiu_Nota_Fiscal = models.CharField(max_length=5, choices=SimOuNao)
     Queixa_do_Paciente = models.CharField(max_length=2000)
-
-class Paciente(models.Model):
-
-    id_paciente = models.AutoField(primary_key=True)
-    nome = models.CharField(primary_key=True,max_length=100, default='')
-    CPF = models.CharField(max_length=14, default='')
-    RG = models.CharField(max_length=14, default='')
-    Data_Nascimento = models.DateField()
-    sexo = models.CharField(max_length=1, choices=SexoPaciente, default=SexoPaciente.outro)
-    contato = models.CharField(max_length=16)
-    logradouro = models.CharField(max_length=100, null=False)
-    lote = models.IntegerField(null=False)
-    bairro = models.CharField(max_length=100, null=False)
-    CEP = models.CharField(max_length=9, null=False)
-    cidade = models.CharField(max_length=100, null=False)
-    estado = models.CharField(max_length=2, null=False)
-    fk_agendamento = models.ForeignKey(Agendamento, on_delete=models.CASCADE, null=True)
-    fk_consulta = models.ForeignKey(Consulta, on_delete=models.CASCADE, null=True)
-
+    paciente_agendamento = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='agendamentos')
     def __str__(self):
-        return f'{self.nome}'
+        return f'{self.paciente_agendamento}'
+
+
+
 
 
 
